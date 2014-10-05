93320cc103485b83800cc95026ba6aa8920ca302
Ian Nastajus <ian.nastajus@gmail.com>
2014-10-04 23:58:48 -0400

    just excel spreadsheet and some empty classes

---
+++ SkillMatrix1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillMatrix1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string fileSkills = "hier.txt";
+            string appPath = Path.Combine( Path.GetDirectoryName( location ), fileSkills );
+
+            //
+	        // It will free resources on its own.
+	        //
+	        string line;
+            using (StreamReader reader = new StreamReader(appPath))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    
+
+                    //count tabs
+                    //new skill
+                    Skill s = new Skill();
+                }
+                line = reader.ReadLine();
+                Console.WriteLine(line);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
